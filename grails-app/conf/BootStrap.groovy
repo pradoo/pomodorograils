cff3a2bcecc8b5b63360e71c60876f2e07871e48
pradoo <prado@utexas.edu>
2014-01-27 23:23:08 -0600

    modified BootStrap.groovy to have preloaded data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,25 @@
+import org.example.pomodoro.Tag
+import org.example.pomodoro.Task
+
 class BootStrap {
 
     def init = { servletContext ->
-    }
+		//creates new Tag and shows error if it does not validate.
+		def workTag = new Tag(name: "Work").save(failOnError:true)
+		def homeTag = new Tag(name: "Home").save(failOnError:true)
+		def schoolTag = new Tag(name: "School").save(failOnError:true)
+		
+		def task = new Task(
+			summary: "Carrer Fair",
+			details: "Frank Erwin Center CNS career fair")
+		/*
+		 * anytime that a domain class has a collection property of
+		 * has many it gains addTo___ and removeFrom___ methods 
+		 */
+		task.addToTags(workTag)
+		task.addToTags(schoolTag)
+		task.save(failOnError: true)
+	}
     def destroy = {
     }
 }
